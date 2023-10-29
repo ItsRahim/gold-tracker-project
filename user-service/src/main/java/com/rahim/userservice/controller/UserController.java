8bb9d3d58893a9497dda3e9fa3806a27d14e3fc5
ItsRahim <rahim1605@gmail.com>
2023-10-29 17:21:19 +0000

    initialised user and user profile controller and repo

---
+++ user-service/src/main/java/com/rahim/userservice/controller/UserController.java
@@ -0,0 +1,7 @@
+package com.rahim.userservice.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UserController {
+}
---
+++ user-service/src/main/java/com/rahim/userservice/controller/UserProfileController.java
@@ -0,0 +1,7 @@
+package com.rahim.userservice.controller;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class UserProfileController {
+}
