80aa88a52cb3e5f1a9bf48ca4e06939e8d833d46
ItsRahim <rahim1605@gmail.com>
2023-10-29 17:20:05 +0000

    generated user profile model from db

---
+++ user-service/src/main/java/com/rahim/userservice/model/UserProfile.java
@@ -0,0 +1,36 @@
+package com.rahim.userservice.model;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "user_profiles", schema = "rgts")
+public class UserProfile {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "profile_id", nullable = false)
+    private Integer id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @Column(name = "username")
+    private String username;
+
+    @Column(name = "first_name")
+    private String firstName;
+
+    @Column(name = "last_name")
+    private String lastName;
+
+    @Column(name = "contact_number", length = 20)
+    private String contactNumber;
+
+    @Column(name = "address", length = Integer.MAX_VALUE)
+    private String address;
+
+}
