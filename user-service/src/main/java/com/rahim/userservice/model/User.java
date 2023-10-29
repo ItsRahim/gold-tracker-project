e4be8e81400f51103f2bf16672e87ab8fe52fecd
ItsRahim <rahim1605@gmail.com>
2023-10-29 17:02:11 +0000

    generated entity from db

---
+++ user-service/src/main/java/com/rahim/userservice/model/User.java
@@ -0,0 +1,50 @@
+package com.rahim.userservice.model;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.Instant;
+import java.time.OffsetDateTime;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "users", schema = "rgts")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "user_id", nullable = false)
+    private Integer id;
+
+    @Column(name = "email", nullable = false)
+    private String email;
+
+    @Column(name = "password_hash", nullable = false)
+    private String passwordHash;
+
+    @Column(name = "account_non_expired")
+    private Boolean accountNonExpired;
+
+    @Column(name = "account_non_locked")
+    private Boolean accountNonLocked;
+
+    @Column(name = "credentials_non_expired")
+    private Boolean credentialsNonExpired;
+
+    @Column(name = "last_login")
+    private Instant lastLogin;
+
+    @Column(name = "notification_setting")
+    private Integer notificationSetting;
+
+    @Column(name = "created_at")
+    private OffsetDateTime createdAt;
+
+    @Column(name = "updated_at")
+    private OffsetDateTime updatedAt;
+
+    @Column(name = "login_attempts")
+    private Integer loginAttempts;
+
+}
