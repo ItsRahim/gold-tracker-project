b51ebd1905ed44ba1459ecc015d182ab1c1dba6f
ItsRahim <rahim1605@gmail.com>
2023-11-05 20:01:16 +0000

    updated service method return types and parameters

--- user-service/src/main/java/com/rahim/userservice/service/IUserProfileService.java
+++ user-service/src/main/java/com/rahim/userservice/service/IUserProfileService.java
@@ -6,8 +6,11 @@ import java.util.List;
 import java.util.Optional;
 
 public interface IUserProfileService {
-    void save(UserProfile userProfile);
-    Optional<UserProfile> findById(int id);
+    void createUserProfile(UserProfile userProfile);
+    Optional<UserProfile> findUserProfileById(int id);
     List<UserProfile> findAllUserProfiles();
-    void updateExistingUserProfile(UserProfile userProfile);
+    void updateUserProfile(UserProfile userProfile);
+    Optional<UserProfile> findByUserProfileByUsername(String username);
+
+    void deleteUserProfile(int userId);
 }
