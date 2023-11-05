a338faedd2e0d812521ef3733aabbfd9a58625fc
ItsRahim <rahim1605@gmail.com>
2023-11-05 20:00:44 +0000

    added queries to repo for service methods

--- user-service/src/main/java/com/rahim/userservice/repository/UserProfileRepository.java
+++ user-service/src/main/java/com/rahim/userservice/repository/UserProfileRepository.java
@@ -2,8 +2,19 @@ package com.rahim.userservice.repository;
 
 import com.rahim.userservice.model.UserProfile;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
 
 @Repository
 public interface UserProfileRepository extends JpaRepository<UserProfile, Integer> {
+
+    @Query(value = "SELECT * FROM rgts.user_profiles WHERE username = :username", nativeQuery = true)
+    Optional<UserProfile> findByUsername(@Param("username") String username);
+
+    @Query(value = "SELECT profile_id FROM rgts.user_profiles WHERE user_id = :id", nativeQuery = true)
+    int getProfileId(@Param("id") int id);
+
 }
