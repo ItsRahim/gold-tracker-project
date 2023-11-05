5a2dad0b0b457108536b41e067f64491610d8382
ItsRahim <rahim1605@gmail.com>
2023-11-05 20:01:51 +0000

    added additional service methods to implement

--- user-service/src/main/java/com/rahim/userservice/service/IUserService.java
+++ user-service/src/main/java/com/rahim/userservice/service/IUserService.java
@@ -1,12 +1,17 @@
 package com.rahim.userservice.service;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.rahim.userservice.model.User;
+import com.rahim.userservice.model.UserRequest;
 
 import java.util.List;
+import java.util.Map;
 import java.util.Optional;
 
 public interface IUserService {
-    void save(User user);
-    Optional<User> findById(int id);
+    void createUserAndProfile(UserRequest userRequest) throws Exception;
+    Optional<User> findUserById(int id) throws Exception;
     List<User> findAllUsers();
+    boolean deleteUserRequest(int id) throws JsonProcessingException;
+    void updateUser(int userId, Map<String, String> updatedData) throws Exception;
 }
