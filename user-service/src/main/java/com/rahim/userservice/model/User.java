718e81fe5572e6d6922a9583b85ab5e2aaba7fd2
ItsRahim <rahim1605@gmail.com>
2023-11-05 19:58:29 +0000

    changed model class to store default field values

--- user-service/src/main/java/com/rahim/userservice/model/User.java
+++ user-service/src/main/java/com/rahim/userservice/model/User.java
@@ -1,9 +1,12 @@
 package com.rahim.userservice.model;
 
-import com.rahim.userservice.enums.AccountStatus;
+import com.rahim.userservice.listener.UserEntityListener;
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.hibernate.annotations.DynamicInsert;
 
 import java.time.Instant;
 import java.time.OffsetDateTime;
@@ -11,6 +14,10 @@ import java.time.OffsetDateTime;
 @Getter
 @Setter
 @Entity
+@DynamicInsert
+@AllArgsConstructor
+@NoArgsConstructor
+@EntityListeners(UserEntityListener.class)
 @Table(name = "users", schema = "rgts")
 public class User {
     @Id
@@ -24,31 +31,45 @@ public class User {
     @Column(name = "password_hash", nullable = false)
     private String passwordHash;
 
-    @Column(name = "account_status", nullable = false)
-    private AccountStatus accountStatus;
+    @Column(name = "account_status", columnDefinition = "VARCHAR(255) DEFAULT 'ACTIVE'")
+    private String accountStatus;
 
-    @Column(name = "account_locked")
+    @Column(name = "account_locked", columnDefinition = "BOOLEAN DEFAULT false")
     private Boolean accountLocked;
 
-    @Column(name = "credentials_expired")
+    @Column(name = "credentials_expired", columnDefinition = "BOOLEAN DEFAULT false")
     private Boolean credentialsExpired;
 
     @Column(name = "last_login")
     private Instant lastLogin;
 
-    @Column(name = "notification_setting")
+    @Column(name = "notification_setting", columnDefinition = "BOOLEAN DEFAULT false")
     private Boolean notificationSetting;
 
-    @Column(name = "created_at")
+    @Column(name = "created_at", columnDefinition = "TIMESTAMPTZ(0) DEFAULT NOW()")
     private OffsetDateTime createdAt;
 
-    @Column(name = "updated_at")
+    @Column(name = "updated_at", columnDefinition = "TIMESTAMPTZ(0) DEFAULT NOW()")
     private OffsetDateTime updatedAt;
 
-    @Column(name = "login_attempts")
+    @Column(name = "login_attempts", columnDefinition = "INT DEFAULT 0")
     private Integer loginAttempts;
 
-    @Column(name = "delete_date")
+    @Column(name = "delete_date", columnDefinition = "TIMESTAMPTZ(0)")
     private OffsetDateTime deleteDate;
 
+    public User(User user) {
+        this.id = user.getId();
+        this.email = user.getEmail();
+        this.passwordHash = user.getPasswordHash();
+        this.accountStatus = user.getAccountStatus();
+        this.accountLocked = user.getAccountLocked();
+        this.credentialsExpired = user.getCredentialsExpired();
+        this.lastLogin = user.getLastLogin();
+        this.notificationSetting = user.getNotificationSetting();
+        this.createdAt = user.getCreatedAt();
+        this.updatedAt = user.getUpdatedAt();
+        this.loginAttempts = user.getLoginAttempts();
+        this.deleteDate = user.getDeleteDate();
+    }
 }
