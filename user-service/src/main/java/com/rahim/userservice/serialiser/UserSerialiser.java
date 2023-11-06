186450d61ab4b6be371d1e9bd414b7e0576a5234
ItsRahim <rahim1605@gmail.com>
2023-11-06 16:50:10 +0000

    changed folder name

--- user-service/src/main/java/com/rahim/userservice/serialiser/UserSerialiser.java
+++ user-service/src/main/java/com/rahim/userservice/serialiser/UserSerialiser.java
@@ -1,4 +1,4 @@
-package com.rahim.userservice.helper;
+package com.rahim.userservice.serialiser;
 
 import com.fasterxml.jackson.core.JsonGenerator;
 import com.fasterxml.jackson.databind.JsonSerializer;
