bc312bc7553b261089abc9e944cf15f2b2a20e14
ItsRahim <rahim1605@gmail.com>
2023-11-15 18:11:16 +0000

    finalised send message method for kafka

--- scheduler-service/src/main/java/com/rahim/schedulerservice/kafka/implementation/KafkaService.java
+++ scheduler-service/src/main/java/com/rahim/schedulerservice/kafka/implementation/KafkaService.java
@@ -2,18 +2,43 @@ package com.rahim.schedulerservice.kafka.implementation;
 
 import com.rahim.schedulerservice.kafka.IKafkaService;
 import lombok.RequiredArgsConstructor;
+import org.apache.kafka.clients.producer.ProducerRecord;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.support.SendResult;
+import org.springframework.stereotype.Service;
 
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
+
+@Service
 @RequiredArgsConstructor
 public class KafkaService implements IKafkaService {
     private static final Logger log = LoggerFactory.getLogger(KafkaService.class);
 
     private final KafkaTemplate<String, String> kafkaTemplate;
+
     @Override
     public void sendMessage(String topic, String data) {
-        log.info("Sending Message to Kafka...");
-        kafkaTemplate.send(topic, data);
+        try {
+            ProducerRecord<String, String> record = new ProducerRecord<>(topic, data);
+
+            CompletableFuture<SendResult<String, String>> future = kafkaTemplate.send(record);
+
+            future.whenComplete((result, ex) -> {
+                if (ex != null) {
+                    log.error("Error sending message to topic '{}': {}", topic, ex.getMessage(), ex);
+                } else {
+                    log.info("Message sent to topic '{}' successfully. Partition: {}, Offset: {}",
+                            topic, result.getRecordMetadata().partition(), result.getRecordMetadata().offset());
+                }
+            });
+
+            future.get();
+        } catch (InterruptedException | ExecutionException e) {
+            log.error("Error occurred in sending message: {}", e.getMessage(), e);
+            throw new RuntimeException(e);
+        }
     }
 }
