203e1170518889649cec81db0fcaeb8217b1cdfb
ItsRahim <rahim1605@gmail.com>
2023-11-18 13:45:20 +0000

    added kafka listener to listen to scheduled jobs

---
+++ user-service/src/main/java/com/rahim/userservice/service/KafkaListenerConfig.java
@@ -0,0 +1,20 @@
+package com.rahim.userservice.service;
+
+import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.annotation.KafkaListener;
+
+@Configuration
+@RequiredArgsConstructor
+public class KafkaListenerConfig {
+    private static final Logger log = LoggerFactory.getLogger(KafkaListenerConfig.class);
+    private final IInternalUserService internalUserService;
+
+    @KafkaListener(topics = "user-cleanup-topic", groupId = "group2")
+    public void cleanupUserAccounts(String message) {
+        log.info("Message received from Scheduler Service: {}", message);
+    }
+}
+
