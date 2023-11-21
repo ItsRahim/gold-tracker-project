3e136b86c05ca8fc44ab3173a70b2eb2fac597af
ItsRahim <rahim1605@gmail.com>
2023-11-21 21:05:15 +0000

    created email history table

---
+++ database/src/main/resources/db/base/V7__CREATE_EMAIL_HISTORY_TABLE.sql
@@ -0,0 +1,7 @@
+CREATE TABLE rgts.email_history (
+    id SERIAL PRIMARY KEY,
+    email_template_id INT REFERENCES rgts.email_template(template_id),
+    recipient_email VARCHAR(255) NOT NULL,
+    sent_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    status VARCHAR(50) NOT NULL
+);
