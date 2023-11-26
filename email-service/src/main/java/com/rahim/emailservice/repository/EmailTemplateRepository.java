796adb13315423ca9d6e5e813282e54ef0040998
ItsRahim <rahim1605@gmail.com>
2023-11-26 11:59:18 +0000

    added missing annotation

--- email-service/src/main/java/com/rahim/emailservice/repository/EmailTemplateRepository.java
+++ email-service/src/main/java/com/rahim/emailservice/repository/EmailTemplateRepository.java
@@ -2,6 +2,8 @@ package com.rahim.emailservice.repository;
 
 import com.rahim.emailservice.model.EmailTemplate;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface EmailTemplateRepository extends JpaRepository<EmailTemplate, Integer> {
 }
