edf1142f3d71352905750fbfcdac0ade5a78efab
ItsRahim <rahim1605@gmail.com>
2023-11-29 17:49:31 +0000

    created table to store gold price

---
+++ database/src/main/resources/db/base/V8__CREATE_GOLD_TYPES_TABLE.sql
@@ -0,0 +1,14 @@
+CREATE TABLE rgts.gold_types (
+    gold_type_id SERIAL PRIMARY KEY,
+    name VARCHAR(50) NOT NULL,
+    net_weight NUMERIC(5, 2) NOT NULL,
+    carat VARCHAR(3) NOT NULL,
+    description TEXT NOT NULL
+);
+
+COMMENT ON TABLE rgts.gold_types IS 'Table containing various gold forms';
+COMMENT ON COLUMN rgts.gold_types.gold_type_id IS 'Unique identifier for different gold types';
+COMMENT ON COLUMN rgts.gold_types.name IS 'The name of the gold item';
+COMMENT ON COLUMN rgts.gold_types.net_weight IS 'The net weight of the gold item';
+COMMENT ON COLUMN rgts.gold_types.carat IS 'The purity of the item (e.g., 18k, 24k)';
+COMMENT ON COLUMN rgts.gold_types.description IS 'The description of the item';
