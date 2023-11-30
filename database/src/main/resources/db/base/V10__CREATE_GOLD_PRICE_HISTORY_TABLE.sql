3bc573ef8188ecca551aff75f8aa4d3c6fe0332d
ItsRahim <rahim1605@gmail.com>
2023-11-30 19:21:49 +0000

    updated gold history table to have closing gold price per gram and ounce

--- database/src/main/resources/db/base/V10__CREATE_GOLD_PRICE_HISTORY_TABLE.sql
+++ database/src/main/resources/db/base/V10__CREATE_GOLD_PRICE_HISTORY_TABLE.sql
@@ -1,12 +1,12 @@
 CREATE TABLE gold_price_history (
     price_id SERIAL PRIMARY KEY,
-    open_price NUMERIC(10, 2) NOT NULL,
-    close_price NUMERIC(10, 2) NOT NULL,
+    price_ounce NUMERIC(10, 2) NOT NULL,
+    price_gram NUMERIC(10, 2) NOT NULL,
     effective_date DATE
 );
 
 COMMENT ON TABLE gold_price_history IS 'Table storing historical records of daily gold prices';
 COMMENT ON COLUMN gold_price_history.price_id IS 'Unique identifier for each daily gold price entry';
-COMMENT ON COLUMN gold_price_history.open_price IS 'The opening gold price on a specific day (in the specified currency)';
-COMMENT ON COLUMN gold_price_history.close_price IS 'The closing gold price on a specific day (in the specified currency)';
+COMMENT ON COLUMN gold_price_history.price_ounce IS 'The closing gold price per troy ounce on a specific day (in the specified currency)';
+COMMENT ON COLUMN gold_price_history.price_gram IS 'The closing gold price per gram on a specific day (in the specified currency)';
 COMMENT ON COLUMN gold_price_history.effective_date IS 'Date indicating when the gold prices are effective';
