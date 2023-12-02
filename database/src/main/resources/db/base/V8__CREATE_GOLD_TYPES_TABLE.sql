4d39790bbe65177838bd62973a1f13fb1ee8dcae
ItsRahim <rahim1605@gmail.com>
2023-12-02 11:56:14 +0000

    added xaugbp to default schema

--- database/src/main/resources/db/base/V8__CREATE_GOLD_TYPES_TABLE.sql
+++ database/src/main/resources/db/base/V8__CREATE_GOLD_TYPES_TABLE.sql
@@ -1,7 +1,7 @@
 CREATE TABLE rgts.gold_types (
     gold_type_id SERIAL PRIMARY KEY,
-    name VARCHAR(50) NOT NULL,
-    net_weight NUMERIC(5, 2) NOT NULL,
+    name VARCHAR(50) NOT NULL UNIQUE,
+    net_weight NUMERIC(5, 2) NULL,
     carat VARCHAR(3) NOT NULL,
     description TEXT NOT NULL
 );
@@ -12,3 +12,5 @@ COMMENT ON COLUMN rgts.gold_types.name IS 'The name of the gold item';
 COMMENT ON COLUMN rgts.gold_types.net_weight IS 'The net weight of the gold item';
 COMMENT ON COLUMN rgts.gold_types.carat IS 'The purity of the item (e.g., 18k, 24k)';
 COMMENT ON COLUMN rgts.gold_types.description IS 'The description of the item';
+
+INSERT INTO rgts.gold_types(name, net_weight, carat, description) VALUES ('XAUGBP', NULL, '24K', 'Pure Gold (24K)')
--- database/src/main/resources/db/base/V9__CREATE_GOLD_PRICES_TABLE.sql
+++ database/src/main/resources/db/base/V9__CREATE_GOLD_PRICES_TABLE.sql
@@ -2,7 +2,7 @@ CREATE TABLE rgts.gold_prices (
     price_id SERIAL PRIMARY KEY,
     gold_type_id INT REFERENCES rgts.gold_types(gold_type_id),
     current_price NUMERIC(5,2) NOT NULL,
-    updated_at TIMESTAMPTZ(0)
+    updated_at TIMESTAMPTZ(0) DEFAULT NOW()
 );
 
 COMMENT ON TABLE rgts.gold_prices IS 'Table storing historical gold prices for different gold types';
@@ -10,3 +10,5 @@ COMMENT ON COLUMN rgts.gold_prices.price_id IS 'Unique identifier for each gold 
 COMMENT ON COLUMN rgts.gold_prices.gold_type_id IS 'Foreign key referencing the gold type associated with the price';
 COMMENT ON COLUMN rgts.gold_prices.current_price IS 'The gold price for a specific gold type (in the specified currency)';
 COMMENT ON COLUMN rgts.gold_prices.updated_at IS 'Timestamp indicating when the gold price entry was last updated';
+
+INSERT INTO rgts.gold_prices(gold_type_id, current_price) VALUES (1, 0);
