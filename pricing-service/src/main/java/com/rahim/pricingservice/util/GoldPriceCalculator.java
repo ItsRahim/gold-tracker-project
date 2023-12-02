4ed376ab31192e45f11c1fce57848b35d78045de
ItsRahim <rahim1605@gmail.com>
2023-12-02 13:46:24 +0000

    added methods to calulate gold price per gram and weight

--- pricing-service/src/main/java/com/rahim/pricingservice/util/GoldPriceCalculator.java
+++ pricing-service/src/main/java/com/rahim/pricingservice/util/GoldPriceCalculator.java
@@ -1,23 +1,46 @@
 package com.rahim.pricingservice.util;
 
+import com.rahim.pricingservice.enums.GoldPurity;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class GoldPriceCalculator {
-    private static final Logger log = LoggerFactory.getLogger(GoldPriceCalculator.class);
+    private static final Logger LOG = LoggerFactory.getLogger(GoldPriceCalculator.class);
+    private double pricePerGram;
 
     public double calculatePricePerGram(double pricePerOunce) {
         try {
             double GRAMS_PER_TROY_OUNCE = 31.1035;
-            double pricePerGram = pricePerOunce / GRAMS_PER_TROY_OUNCE;
+            pricePerGram = pricePerOunce / GRAMS_PER_TROY_OUNCE;
 
-            log.info("Conversion successful: {} per ounce is {} per gram", pricePerOunce, pricePerGram);
+            LOG.info("Conversion successful: {} per ounce is {} per gram", pricePerOunce, pricePerGram);
 
             return pricePerGram;
         } catch (ArithmeticException ex) {
-            log.error("Error converting price per ounce to per gram: {}", ex.getMessage());
+            LOG.error("Error converting price per ounce to per gram: {}", ex.getMessage());
             throw new RuntimeException("Error converting price per ounce to per gram", ex);
         }
     }
 
+    public double calculateGoldPriceByCarat(String carat) {
+        try {
+            double purity = GoldPurity.getPurityByCarat(carat);
+            return purity * pricePerGram;
+        } catch (IllegalArgumentException e) {
+            LOG.error("Error calculating gold price for carat {}: {}", carat, e.getMessage());
+            return 0;
+        }
+    }
+
+    public double calculateGoldPrice(double caratPurity, double netWeight, double pricePerGram) {
+        if (caratPurity < 0 || caratPurity > 1 || netWeight < 0 || pricePerGram < 0) {
+            LOG.error("Invalid input values. Carat purity: {}, Net weight: {}, Price per gram: {}", caratPurity, netWeight, pricePerGram);
+            throw new IllegalArgumentException("Invalid input values. Carat purity, net weight, and price per gram must be non-negative.");
+        }
+
+        double goldPrice = caratPurity * netWeight * pricePerGram;
+        LOG.info("Calculated Gold Price: Carat Purity: {}, Net Weight: {} grams, Price per gram: ${}, Total Price: ${}", caratPurity, netWeight, pricePerGram, goldPrice);
+
+        return goldPrice;
+    }
 }
