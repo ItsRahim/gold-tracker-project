519feb2b3f99a59d593795b91d847c4b1c305e1a
ItsRahim <rahim1605@gmail.com>
2023-12-03 12:44:32 +0000

    moved all listeners back into one calss

--- pricing-service/src/main/java/com/rahim/pricingservice/kafka/KafkaListenerConfig.java
+++ pricing-service/src/main/java/com/rahim/pricingservice/kafka/KafkaListenerConfig.java
@@ -1,16 +1,33 @@
 package com.rahim.pricingservice.kafka;
 
+import com.rahim.pricingservice.service.IGoldPriceService;
+import com.rahim.pricingservice.service.feign.IGoldPriceFeignClient;
+import lombok.RequiredArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.annotation.KafkaListener;
 
 @Configuration
+@RequiredArgsConstructor
 public class KafkaListenerConfig {
     private static final Logger LOG = LoggerFactory.getLogger(KafkaListenerConfig.class);
+    private final IGoldPriceFeignClient goldPriceFeignClient;
+    private final IGoldPriceService goldPriceService;
 
     @KafkaListener(topics = "pricing-service-update-prices", groupId = "group2")
     public void updateGoldPriceJob(String message) {
         LOG.info("Message received from Scheduler Service: {}", message);
+        goldPriceFeignClient.getGoldPrice();
+    }
+
+    @KafkaListener(topics = "gold-price-stream", groupId = "group2")
+    public void processPriceChange(String priceData) {
+        if (priceData.isEmpty()) {
+            LOG.error("No gold price data received from the custom pricing API.");
+        } else {
+            LOG.info("Successfully received the latest gold prices. Processing data...");
+            goldPriceService.setKafkaData(priceData);
+        }
     }
 }
