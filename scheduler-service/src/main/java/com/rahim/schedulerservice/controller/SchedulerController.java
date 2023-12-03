592e0fdf379ff8287420b02c7ef25705365531f2
ItsRahim <rahim1605@gmail.com>
2023-12-03 12:17:29 +0000

    changed response body type and url

--- scheduler-service/src/main/java/com/rahim/schedulerservice/controller/SchedulerController.java
+++ scheduler-service/src/main/java/com/rahim/schedulerservice/controller/SchedulerController.java
@@ -3,29 +3,44 @@ package com.rahim.schedulerservice.controller;
 import com.rahim.schedulerservice.model.TimerInfo;
 import com.rahim.schedulerservice.service.ISchedulerService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/v1/gold/jobs")
+@RequestMapping("/api/v1/gold/scheduler-service/jobs")
 @RequiredArgsConstructor
 public class SchedulerController {
 
     private final ISchedulerService schedulerService;
 
     @GetMapping
-    public List<TimerInfo> getAllRunningTimers() {
-        return schedulerService.getAllRunningTimers();
+    public ResponseEntity<List<TimerInfo>> getAllRunningTimers() {
+        List<TimerInfo> runningTimers = schedulerService.getAllRunningTimers();
+        return ResponseEntity.status(HttpStatus.OK).body(runningTimers);
     }
 
     @GetMapping("/{timerId}")
-    public TimerInfo getRunningTimer(@PathVariable String timerId) {
-        return schedulerService.getRunningTimer(timerId);
+    public ResponseEntity<TimerInfo> getRunningTimer(@PathVariable String timerId) {
+        TimerInfo runningTimer = schedulerService.getRunningTimer(timerId);
+
+        if (runningTimer != null) {
+            return ResponseEntity.status(HttpStatus.OK).body(runningTimer);
+        } else {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+        }
     }
 
     @DeleteMapping("/{timerId}")
-    public Boolean deleteTimer(@PathVariable String timerId) {
-        return schedulerService.deleteTimer(timerId);
+    public ResponseEntity<Boolean> deleteTimer(@PathVariable String timerId) {
+        Boolean deletionStatus = schedulerService.deleteTimer(timerId);
+
+        if (deletionStatus != null && deletionStatus) {
+            return ResponseEntity.status(HttpStatus.OK).body(true);
+        } else {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+        }
     }
 }
