e11e4dfc9850bd19f0556df7c57eb1201f394856
ItsRahim <rahim1605@gmail.com>
2023-12-03 12:45:06 +0000

    fix feign to call correct endpoint

--- pricing-service/src/main/java/com/rahim/pricingservice/service/feign/IGoldPriceFeignClient.java
+++ pricing-service/src/main/java/com/rahim/pricingservice/service/feign/IGoldPriceFeignClient.java
@@ -4,10 +4,10 @@ import com.rahim.pricingservice.config.GoldPriceFeignClientConfig;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 
-@FeignClient(name = "gold-price-api-client", configuration = GoldPriceFeignClientConfig.class)
+@FeignClient(name = "gold-price-api-client", configuration = GoldPriceFeignClientConfig.class, url = "${gold-price-api.url}")
 public interface IGoldPriceFeignClient {
 
-    @GetMapping("/api/v1/gold/uk-investing")
+    @GetMapping()
     String getGoldPrice();
 }
 
