bf6e2af224cf752fff2bc2fe0cc34cc1aa110eb8
ItsRahim <rahim1605@gmail.com>
2023-12-19 19:22:51 +0000

    initial commit for notifications

---
+++ notification-service/src/main/java/com/rahim/notificationservice/listener/ThresholdAlertEntityListener.java
@@ -0,0 +1,7 @@
+package com.rahim.notificationservice.listener;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class ThresholdAlertEntityListener {
+}
---
+++ notification-service/src/main/java/com/rahim/notificationservice/model/ThresholdAlert.java
@@ -0,0 +1,32 @@
+package com.rahim.notificationservice.model;
+
+import com.rahim.notificationservice.listener.ThresholdAlertEntityListener;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+
+@Getter
+@Setter
+@Entity
+@RequiredArgsConstructor
+@EntityListeners(ThresholdAlertEntityListener.class)
+@Table(name = "threshold_alerts", schema = "rgts")
+public class ThresholdAlert {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "alert_id", nullable = false)
+    private Integer id;
+
+    @Column(name = "user_id")
+    private Integer user;
+
+    @Column(name = "threshold_price", nullable = false, precision = 10, scale = 2)
+    private BigDecimal thresholdPrice;
+
+    @Column(name = "is_active")
+    private Boolean isActive;
+
+}
