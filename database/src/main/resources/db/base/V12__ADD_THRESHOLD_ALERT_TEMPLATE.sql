44ffbec8979dd09a9a27b8df3991582e07764db3
ItsRahim <rahim1605@gmail.com>
2023-12-19 21:45:46 +0000

    initial version of notification

--- database/src/main/resources/db/base/V12__ADD_THRESHOLD_ALERT_TEMPLATE.sql
+++ database/src/main/resources/db/base/V12__ADD_THRESHOLD_ALERT_TEMPLATE.sql
@@ -4,8 +4,8 @@ VALUES (
         'Gold Price Alert: Threshold Reached',
         'Dear @firstName @lastName,\n' ||
         '\nWe hope this message finds you well. This is a notification regarding the gold price at our service.\n' ||
-        'We want to inform you that the price of gold per ounce has reached a threshold value. As of @alertDateTime, the current price is @$goldPrice per ounce.\n' ||
+        'We want to inform you that the price of gold per ounce has reached a threshold value. As of @alertDateTime, the current price is £@thresholdPrice per ounce.\n' ||
         'If you have any concerns or questions, please review your account for more details. We understand the importance of staying informed about market changes, and we are here to assist you with any queries you may have.\n' ||
         '\nThank you for choosing our service.',
-        ARRAY['@firstName', '@lastName', '@alertDateTime', '@goldPrice']
+        ARRAY['@firstName', '@lastName', '@alertDateTime', '@thresholdPrice']
        );
--- notification-service/src/main/java/com/rahim/notificationservice/repository/ThresholdAlertRepository.java
+++ notification-service/src/main/java/com/rahim/notificationservice/repository/ThresholdAlertRepository.java
@@ -2,8 +2,20 @@ package com.rahim.notificationservice.repository;
 
 import com.rahim.notificationservice.model.ThresholdAlert;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.math.BigDecimal;
+import java.util.Map;
+import java.util.Optional;
 
 @Repository
 public interface ThresholdAlertRepository extends JpaRepository<ThresholdAlert, Integer> {
+    @Query(value = "SELECT up.first_name, up.last_name, u.email, ta.threshold_price " +
+            "FROM rgts.user_profiles up " +
+            "JOIN rgts.threshold_alerts ta ON up.user_id = ta.user_id " +
+            "JOIN rgts.users u ON up.user_id = u.user_id " +
+            "WHERE ta.threshold_price = :threshold_price", nativeQuery = true)
+    Optional<Map<String, Object>> getEmailTokens(@Param("threshold_price") BigDecimal threshold_price);
 }
