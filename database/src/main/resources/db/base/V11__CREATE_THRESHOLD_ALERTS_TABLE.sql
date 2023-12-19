e00d8fb47122b9525448d1535f273726aeae3c9f
ItsRahim <rahim1605@gmail.com>
2023-12-19 19:14:55 +0000

    added missing comment on column

--- database/src/main/resources/db/base/V11__CREATE_THRESHOLD_ALERTS_TABLE.sql
+++ database/src/main/resources/db/base/V11__CREATE_THRESHOLD_ALERTS_TABLE.sql
@@ -9,5 +9,6 @@ CREATE INDEX idx_user_id ON rgts.threshold_alerts(user_id);
 
 COMMENT ON TABLE rgts.threshold_alerts is 'Table storing price alerts for users';
 COMMENT ON COLUMN rgts.threshold_alerts.alert_id IS 'The unique ID for each alert';
+COMMENT ON COLUMN rgts.threshold_alerts.user_id IS 'The user ID which set the alert';
 COMMENT ON COLUMN rgts.threshold_alerts.threshold_price IS 'The price at which the user wants to be notified';
 COMMENT ON COLUMN rgts.threshold_alerts.is_active IS 'The flag indicating if the alert is active';
