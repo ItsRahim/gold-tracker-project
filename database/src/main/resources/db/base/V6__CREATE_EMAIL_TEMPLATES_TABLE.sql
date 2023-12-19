0b348841a94d87f0ca894f6fb3664a8d2bf00265
ItsRahim <rahim1605@gmail.com>
2023-12-19 19:00:42 +0000

    change to format new lines correctly

--- database/src/main/resources/db/base/V6__CREATE_EMAIL_TEMPLATES_TABLE.sql
+++ database/src/main/resources/db/base/V6__CREATE_EMAIL_TEMPLATES_TABLE.sql
@@ -17,11 +17,11 @@ INSERT INTO rgts.email_template(template_name, subject, body, placeholders)
 VALUES (
         'Account Deletion',
         'Account Deletion Warning',
-        'Dear @firstName @lastName,' ||
-        'We hope this message finds you well. This is a notification regarding your account at our service.' ||
-        'We want to inform you that based on our records, your account, @username, is scheduled for deletion in 30 days, on @deletionDate. If you have any concerns or questions, please contact our support team as soon as possible.' ||
-        'We value your presence with us, and we''d be happy to assist you with any issues you may have. To prevent the deletion of your account, make sure to log in within the next 30 days, before @deletionDate.' ||
-        'Thank you for being a part of our community.',
+        'Dear @firstName @lastName,\n' ||
+        '\nWe hope this message finds you well. This is a notification regarding your account at our service.\n' ||
+        'We want to inform you that based on our records, your account, @username, is scheduled for deletion in 30 days, on @deletionDate. If you have any concerns or questions, please contact our support team as soon as possible.\n' ||
+        'We value your presence with us, and we''d be happy to assist you with any issues you may have. To prevent the deletion of your account, make sure to log in within the next 30 days, before @deletionDate.\n' ||
+        '\nThank you for being a part of our community.',
         ARRAY['@firstName', '@lastName', '@username', '@deletionDate']
        );
 
@@ -29,11 +29,11 @@ INSERT INTO rgts.email_template(template_name, subject, body, placeholders)
 VALUES (
         'Account Inactivity',
         'Account Inactivity Alert',
-        'Dear @firstName @lastName,' ||
-        'We hope you are well. This is a notice regarding your account on our platform.' ||
-        'Our records indicate that your account has been inactive for the past 30 days. To ensure the security of our system, we kindly request you to log in within the next 14 days. Failure to do so will result in an automatic request to delete your account.' ||
-        'If you have any concerns or questions, please reach out to our support team. We value your continued presence and would like to assist you in any way we can.' ||
-        'Thank you for being a part of our community.',
+        'Dear @firstName @lastName,\n' ||
+        '\nWe hope you are well. This is a notice regarding your account on our platform.\n' ||
+        'Our records indicate that your account has been inactive for the past 30 days. To ensure the security of our system, we kindly request you to log in within the next 14 days. Failure to do so will result in an automatic request to delete your account.\n' ||
+        'If you have any concerns or questions, please reach out to our support team. We value your continued presence and would like to assist you in any way we can.\n' ||
+        '\nThank you for being a part of our community.',
         ARRAY['@firstName', '@lastName']
        );
 
@@ -41,8 +41,8 @@ INSERT INTO rgts.email_template(template_name, subject, body, placeholders)
 VALUES (
         'Account Deleted',
         'Account Deletion Confirmation',
-        'Dear @firstName @lastName,' ||
-        'We hope this message finds you well. This is to inform you that your account, @username, at our service has been successfully deleted.' ||
-        'If you believe this is an error or if you have any questions, please contact our support team for further assistance.',
+        'Dear @firstName @lastName,\n' ||
+        '\nWe hope this message finds you well. This is to inform you that your account, @username, at our service has been successfully deleted.\n' ||
+        '\nIf you believe this is an error or if you have any questions, please contact our support team for further assistance.',
         ARRAY['@firstName', '@lastName', '@username']
        );
