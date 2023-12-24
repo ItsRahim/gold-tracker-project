ae572bd45d42667a051f36401caede261d989a0f
ItsRahim <rahim1605@gmail.com>
2023-12-24 20:01:07 +0000

    removed beans for testing

--- notification-service/src/main/java/com/rahim/notificationservice/NotificationServiceApplication.java
+++ notification-service/src/main/java/com/rahim/notificationservice/NotificationServiceApplication.java
@@ -1,26 +1,15 @@
 package com.rahim.notificationservice;
 
-import com.rahim.notificationservice.service.IThresholdService;
-import lombok.RequiredArgsConstructor;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 @EnableDiscoveryClient
-@RequiredArgsConstructor
 public class NotificationServiceApplication {
-    private final IThresholdService thresholdService;
 
     public static void main(String[] args) {
         SpringApplication.run(NotificationServiceApplication.class, args);
-    }
-
-    @Bean
-    CommandLineRunner commandLineRunner () {
-        return args -> thresholdService.processKafkaData("1600.98");
     }
 
 }
