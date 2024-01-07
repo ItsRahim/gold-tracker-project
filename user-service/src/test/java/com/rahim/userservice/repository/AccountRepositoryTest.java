5e059d9823bf9a24957b5d34db0f6171ef59747d
ItsRahim <rahim1605@gmail.com>
2024-01-07 12:30:22 +0000

    added unit tests for CRUD

--- user-service/src/test/java/com/rahim/userservice/repository/AccountRepositoryTest.java
+++ user-service/src/test/java/com/rahim/userservice/repository/AccountRepositoryTest.java
@@ -3,32 +3,31 @@ package com.rahim.userservice.repository;
 import com.rahim.userservice.model.Account;
 import org.flywaydb.core.Flyway;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.TestPropertySource;
-import org.springframework.test.context.jdbc.Sql;
 import org.testcontainers.containers.KafkaContainer;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
 import org.testcontainers.containers.PostgreSQLContainer;
-import org.testcontainers.shaded.org.checkerframework.checker.units.qual.A;
 import org.testcontainers.utility.DockerImageName;
 
-import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import java.util.Optional;
 
-@ActiveProfiles("test")
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
 @Testcontainers
-@SpringBootTest(properties = {"spring.cloud.config.enabled=false", "spring.cloud.vault.enabled=false", "spring.cloud.discovery.enabled = false"})
+@ActiveProfiles("test")
 @TestPropertySource("classpath:application-test.yaml")
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+@SpringBootTest(properties = {"spring.cloud.config.enabled=false", "spring.cloud.vault.enabled=false", "spring.cloud.discovery.enabled=false"})
 public class AccountRepositoryTest {
 
     @Autowired
@@ -55,26 +54,85 @@ public class AccountRepositoryTest {
         flyway.migrate();
     }
 
+    @BeforeEach
+    void setupTestData() {
+        accountRepository.deleteAll();
+
+        for(int i = 0; i < 10; i++) {
+            Account account = new Account("user" + i + "@example.com", "password" + i);
+            accountRepository.save(account);
+        }
+    }
+
     @Test
     @DisplayName("Testing PostgreSQL Connection")
     void databaseConnectionEstablished() {
-        assertThat(postgresContainer.isCreated()).isTrue();
-        assertThat(postgresContainer.isRunning()).isTrue();
+        assertTrue(postgresContainer.isCreated());
+        assertTrue(postgresContainer.isRunning());
     }
 
     @Test
     @DisplayName("Testing Kafka Connection")
     void kafkaConnectionEstablished() {
-        assertThat(kafkaContainer.isCreated()).isTrue();
-        assertThat(kafkaContainer.isRunning()).isTrue();
+        assertTrue(kafkaContainer.isCreated());
+        assertTrue(kafkaContainer.isRunning());
     }
 
     @Test
-    @DisplayName("Successful Save")
-    void shouldSaveNewAccount() {
+    @DisplayName("Save New Account - Successful")
+    void saveNewAccount_SuccessfulSave() {
         Account account = new Account("jane.doe@gmail.com", "password1");
         accountRepository.save(account);
 
         assertTrue(accountRepository.existsAccountByEmail("jane.doe@gmail.com"));
+        assertEquals(accountRepository.findAll().size(), 11);
     }
+
+    @Test
+    @DisplayName("Remove Account By ID - Successful Removal")
+    void removeAccountById_SuccessfulRemoval() {
+        accountRepository.deleteById(6);
+        Optional<Account> removedAccount = accountRepository.findById(6);
+
+        assertTrue(removedAccount.isEmpty());
+    }
+
+    @Test
+    @DisplayName("Save and Retrieve Account")
+    void saveAndRetrieveAccount() {
+        Account account = new Account("rahim.ahmed@gmail.com", "Password123!");
+        accountRepository.save(account);
+
+        Optional<Account> retrievedAccount = accountRepository.findById(account.getId());
+
+        assertTrue(retrievedAccount.isPresent());
+        assertEquals(account.getEmail(), retrievedAccount.get().getEmail());
+    }
+
+    @Test
+    @DisplayName("Find Non-Existent Account By ID")
+    void shouldNotFindNonExistentAccount() {
+        Optional<Account> optionalAccount = accountRepository.findById(22);
+
+        assertTrue(optionalAccount.isEmpty());
+    }
+
+    @Test
+    @DisplayName("Update Existing Account")
+    void shouldUpdateExistingAccount() {
+        Optional<Account> accountOptional = accountRepository.findById(3);
+        assertTrue(accountOptional.isPresent());
+
+        Account account = accountOptional.get();
+
+        account.setEmail("newemail@gmail.com");
+        accountRepository.save(account);
+
+        Optional<Account> updatedAccountOptional = accountRepository.findById(3);
+        assertTrue(updatedAccountOptional.isPresent());
+
+        Account updatedAccount = updatedAccountOptional.get();
+        assertEquals("newemail@gmail.com", updatedAccount.getEmail());
+    }
+
 }
