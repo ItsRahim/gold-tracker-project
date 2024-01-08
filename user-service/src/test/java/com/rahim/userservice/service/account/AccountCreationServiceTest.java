f60653339f004a492af86f3de12533dcc7d91b35
ItsRahim <rahim1605@gmail.com>
2024-01-08 14:54:06 +0000

    Âadded test for same username and email

--- user-service/src/test/java/com/rahim/userservice/service/account/AccountCreationServiceTest.java
+++ user-service/src/test/java/com/rahim/userservice/service/account/AccountCreationServiceTest.java
@@ -6,8 +6,10 @@ import com.rahim.userservice.model.Account;
 import com.rahim.userservice.model.Profile;
 import com.rahim.userservice.model.UserRequest;
 import com.rahim.userservice.repository.AccountRepository;
+import com.rahim.userservice.repository.ProfileRepository;
 import org.flywaydb.core.Flyway;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -38,6 +40,9 @@ public class AccountCreationServiceTest {
     @Autowired
     AccountRepository accountRepository;
 
+    @Autowired
+    ProfileRepository profileRepository;
+
     @Container
     @ServiceConnection
     static PostgreSQLContainer<?> postgresContainer = new PostgreSQLContainer<>(ContainerImage.POSTGRES.getDockerImageName());
@@ -58,6 +63,12 @@ public class AccountCreationServiceTest {
                 .load();
 
         flyway.migrate();
+    }
+
+    @BeforeEach
+    void cleanupTestData() {
+        profileRepository.deleteAll();
+        accountRepository.deleteAll();
     }
 
     @Test
@@ -113,6 +124,21 @@ public class AccountCreationServiceTest {
         accountCreationService.createAccount(userRequest1);
 
         Account account2 = new Account("anotherUnique@gmail.com", "NewPassword012");
+        Profile profile2 = new Profile(account2, "duplicateUsername", "Bob", "Johnson", "07123456789", "35 Bond Street");
+        UserRequest userRequest2 = new UserRequest(account2, profile2);
+
+        assertThrows(DuplicateAccountException.class, () -> accountCreationService.createAccount(userRequest2));
+    }
+
+    @Test
+    @DisplayName("Fail To Save Duplicate Account - Same Email and Username")
+    void failToSaveDuplicateAccount_SameEmailAndUsername() {
+        Account account1 = new Account("unique@gmail.com", "Password789!");
+        Profile profile1 = new Profile(account1, "duplicateUsername", "Alice", "Smith", "07123456789", "30 Bond Street");
+        UserRequest userRequest1 = new UserRequest(account1, profile1);
+        accountCreationService.createAccount(userRequest1);
+
+        Account account2 = new Account("unique@gmail.com", "NewPassword012");
         Profile profile2 = new Profile(account2, "duplicateUsername", "Bob", "Johnson", "07123456789", "35 Bond Street");
         UserRequest userRequest2 = new UserRequest(account2, profile2);
 
