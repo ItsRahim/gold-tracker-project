34f545c78f1a6b559638b3fb3bb9b2a7ae578e24
ItsRahim <rahim1605@gmail.com>
2024-01-11 13:35:30 +0000

    simplified method

--- user-service/src/main/java/com/rahim/userservice/service/account/implementation/AccountCreationService.java
+++ user-service/src/main/java/com/rahim/userservice/service/account/implementation/AccountCreationService.java
@@ -12,7 +12,6 @@ import lombok.RequiredArgsConstructor;
 import org.apache.commons.lang3.ObjectUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -38,15 +37,9 @@ public class AccountCreationService implements IAccountCreationService {
         boolean anyNullUser = ObjectUtils.anyNull(account);
         boolean anyNullProfile = ObjectUtils.anyNull(profile);
 
-        if (accountExists && profileExists) {
-            LOG.warn("Account with email {} and username {} already exists. Not creating duplicate.", email, username);
+        if (accountExists || profileExists) {
+            LOG.warn("Account with email {} and/or username {} already exists. Not creating duplicate.", email, username);
             throw new DuplicateAccountException("Account with email " + email + " and username " + username + " already exists.");
-        } else if (accountExists) {
-            LOG.warn("Account with email {} already exists. Not creating duplicate.", email);
-            throw new DuplicateAccountException("Account with email " + email + " already exists.");
-        } else if (profileExists) {
-            LOG.warn("Account with username {} already exists. Not creating duplicate.", username);
-            throw new DuplicateAccountException("Account with username " + username + " already exists.");
         }
 
         if (!anyNullUser || !anyNullProfile) {
@@ -55,16 +48,13 @@ public class AccountCreationService implements IAccountCreationService {
                 profileCreation.createProfile(account, profile);
 
                 LOG.info("Successfully created Account and Account Profile for: {}", profile.getUsername());
-            } catch (DataIntegrityViolationException e) {
-                LOG.error("Error creating Account and Account Profile. Data integrity violation: {}", e.getMessage());
-                throw new DuplicateAccountException("Account with email " + email + " or username " + username + " already exists.", e);
             } catch (Exception e) {
                 LOG.error("Unexpected error creating Account and Account Profile: {}", e.getMessage());
                 throw new RuntimeException("Unexpected error creating Account and Account Profile.", e);
             }
         } else {
             LOG.warn("Null values found. Not creating duplicate.");
-            throw new DuplicateAccountException("Null values found in Account or Profile.");
+            throw new NullPointerException("Null values found in Account or Profile.");
         }
     }
 
