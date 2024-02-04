4f5006806ce8b68b50388552d2b8b6d6899bb81b
ItsRahim <rahim1605@gmail.com>
2024-02-04 18:31:38 +0000

    used builder instead

--- batch-service/src/main/java/com/rahim/batchimport/config/BatchConfig.java
+++ batch-service/src/main/java/com/rahim/batchimport/config/BatchConfig.java
@@ -21,6 +21,7 @@ import org.springframework.batch.item.file.transform.DelimitedLineTokenizer;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.task.SimpleAsyncTaskExecutorBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.Resource;
@@ -70,10 +71,9 @@ public class BatchConfig extends AbstractBatchConfig {
 
     @Bean
     public TaskExecutor taskExecutor() {
-        SimpleAsyncTaskExecutor asyncTaskExecutor = new SimpleAsyncTaskExecutor();
-        asyncTaskExecutor.setConcurrencyLimit(100);
-
-        return asyncTaskExecutor;
+        return new SimpleAsyncTaskExecutorBuilder()
+                .concurrencyLimit(100)
+                .build();
     }
 
     @Bean
