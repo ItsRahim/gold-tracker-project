71456c64051ce78538e1c50489057b9573be0979
ItsRahim <rahim1605@gmail.com>
2024-02-11 12:46:58 +0000

    reverted to older version

--- batch-service/src/main/java/com/rahim/batchimport/processor/GoldDataProcessor.java
+++ batch-service/src/main/java/com/rahim/batchimport/processor/GoldDataProcessor.java
@@ -1,14 +1,11 @@
 package com.rahim.batchimport.processor;
 
-import com.rahim.batchimport.exception.DuplicateEffectiveDateException;
 import com.rahim.batchimport.model.GoldData;
 import com.rahim.batchimport.model.GoldPriceHistory;
 import com.rahim.batchimport.util.GoldPriceCalculator;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.batch.item.ItemProcessor;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.jdbc.core.JdbcTemplate;
 
 import java.math.BigDecimal;
 import java.time.LocalDate;
@@ -18,9 +15,6 @@ public class GoldDataProcessor implements ItemProcessor<GoldData, GoldPriceHisto
 
     private static final Logger LOG = LoggerFactory.getLogger(GoldDataProcessor.class);
 
-    @Autowired
-    private JdbcTemplate jdbcTemplate;
-
     @Override
     public GoldPriceHistory process(GoldData goldData) {
         try {
@@ -29,22 +23,11 @@ public class GoldDataProcessor implements ItemProcessor<GoldData, GoldPriceHisto
 
             BigDecimal pricePerGram = GoldPriceCalculator.getInstance().calculatePricePerGram(priceOunce);
 
-            if (isEffectiveDateExists(effectiveDate)) {
-                throw new DuplicateEffectiveDateException("Duplicate effective date found: " + effectiveDate);
-            }
-
             return new GoldPriceHistory(priceOunce, pricePerGram, effectiveDate);
         } catch (NumberFormatException | DateTimeParseException e) {
             LOG.error("Error processing GoldData: {}", goldData, e);
             return null;
         }
-    }
-
-    private boolean isEffectiveDateExists(LocalDate effectiveDate) {
-        String sql = "SELECT EXISTS(SELECT 1 FROM rgts.gold_price_history WHERE effective_date = ?)";
-        Boolean result = jdbcTemplate.queryForObject(sql, Boolean.class, effectiveDate);
-
-        return Boolean.TRUE.equals(result);
     }
 
 }
