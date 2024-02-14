4c327e87fa0204c674d7c60e101d22b916dd348a
ItsRahim <rahim1605@gmail.com>
2024-02-14 20:34:17 +0000

    update

--- batch-service/src/main/java/com/rahim/batchimport/model/GoldPriceHistory.java
+++ batch-service/src/main/java/com/rahim/batchimport/model/GoldPriceHistory.java
@@ -3,12 +3,14 @@ package com.rahim.batchimport.model;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import lombok.ToString;
 
 import java.math.BigDecimal;
 import java.time.LocalDate;
 
 @Getter
 @Setter
+@ToString
 @NoArgsConstructor
 public class GoldPriceHistory {
     private Integer id;
