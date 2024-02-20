b0580b3b693fb5db8dc4d8de1e3c4dfd650a3231
ItsRahim <rahim1605@gmail.com>
2024-02-20 20:49:05 +0000

    fixed serialisatio bug

--- account-service/src/main/java/com/rahim/accountservice/model/Account.java
+++ account-service/src/main/java/com/rahim/accountservice/model/Account.java
@@ -33,8 +33,8 @@ public class Account {
     @JsonProperty("email")
     private String email;
 
-    @JsonIgnore
     @Column(name = "password_hash", nullable = false)
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
     private String passwordHash;
 
     @Column(name = "account_status")
