9d3411d5b5e49f448d3238c826092e3fa8fe85d9
ItsRahim <rahim1605@gmail.com>
2024-02-25 15:58:32 +0000

    Revert "removed unused classes"
    This reverts commit 19b435fbd9aa29f40e44558c54611aee8b90c90d.

---
+++ account-service/src/main/java/com/rahim/accountservice/enums/TemplateNameEnum.java
@@ -0,0 +1,33 @@
+package com.rahim.accountservice.enums;
+
+import lombok.Getter;
+
+/**
+ * This enumeration contains names of email templates.
+ * These templates are used by methods that trigger an email notification to a user about a specific event.
+ * Each constant in this enum represents a unique email template.
+ */
+@Deprecated
+@Getter
+public enum TemplateNameEnum {
+    ACCOUNT_DELETED("Account Deleted"),
+    ACCOUNT_DELETION("Account Deletion"),
+    ACCOUNT_INACTIVITY("Account Inactivity"),
+    ACCOUNT_UPDATE("Account Update");
+
+    private final String templateName;
+
+    TemplateNameEnum(String templateName) {
+        this.templateName = templateName;
+    }
+
+    public static TemplateNameEnum fromTemplateName(String templateName) {
+        for (TemplateNameEnum value : values()) {
+            if (value.templateName.equals(templateName)) {
+                return value;
+            }
+        }
+        throw new IllegalArgumentException("Unknown template name: " + templateName);
+    }
+}
+
---
+++ account-service/src/main/java/com/rahim/accountservice/serialiser/UserProfileSerialiser.java
@@ -0,0 +1,24 @@
+package com.rahim.accountservice.serialiser;
+
+import com.fasterxml.jackson.core.JsonGenerator;
+import com.fasterxml.jackson.databind.JsonSerializer;
+import com.fasterxml.jackson.databind.SerializerProvider;
+import com.rahim.accountservice.model.Profile;
+
+import java.io.IOException;
+
+@Deprecated
+public class UserProfileSerialiser extends JsonSerializer<Profile> {
+
+    public void serialize(Profile profile, JsonGenerator jsonGenerator, SerializerProvider serializerProvider) throws IOException {
+        jsonGenerator.writeStartObject();
+        jsonGenerator.writeObjectField("id", profile.getId());
+        jsonGenerator.writeStringField("username", profile.getUsername());
+        jsonGenerator.writeStringField("firstName", profile.getFirstName());
+        jsonGenerator.writeStringField("lastName", profile.getLastName());
+        jsonGenerator.writeStringField("contactNumber", profile.getContactNumber());
+        jsonGenerator.writeStringField("address", profile.getAddress());
+        jsonGenerator.writeEndObject();
+    }
+}
+
