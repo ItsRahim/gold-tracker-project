1ff141c3e8d38edec1a9bfa46d9a924cdf03f9bf
ItsRahim <rahim1605@gmail.com>
2024-04-13 19:54:22 +0100

    Initial commit for gate way service

--- gateway-service/src/test/java/com/rahim/gatewayservice/GatewayServiceApplicationTests.java
+++ gateway-service/src/test/java/com/rahim/gatewayservice/GatewayServiceApplicationTests.java
@@ -1,10 +1,10 @@
-package com.rahim.authservice;
+package com.rahim.gatewayservice;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class AuthServiceApplicationTests {
+class GatewayServiceApplicationTests {
 
 	@Test
 	void contextLoads() {
