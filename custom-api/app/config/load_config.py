d8e9613cf04ac7c9e85c3340076a6cf2630fe94e
ItsRahim <rahim1605@gmail.com>
2024-04-14 13:08:46 +0100

    adedd git ignore and new method to load vault config

--- custom-api/app/config/load_config.py
+++ custom-api/app/config/load_config.py
@@ -4,3 +4,8 @@ import yaml
 def load_config(element: str) -> str:
     with open('app/resources/app_config.yaml', 'r') as config_file:
         return yaml.safe_load(config_file)[element]
+
+
+def load_vault_config(element: str) -> str:
+    with open('app/resources/vault_config.yaml', 'r') as vault_config:
+        return yaml.safe_load(vault_config)[element]
