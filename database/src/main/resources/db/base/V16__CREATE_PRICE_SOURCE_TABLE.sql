802dead3678c7be123473f823d76014cf97fc6ab
ItsRahim <rahim1605@gmail.com>
2024-04-17 23:04:49 +0100

    added new table for pyhton data sources

---
+++ database/src/main/resources/db/base/V16__CREATE_PRICE_SOURCE_TABLE.sql
@@ -0,0 +1,14 @@
+CREATE TABLE rgts.price_sources (
+    source_id SERIAL PRIMARY KEY,
+    source_name VARCHAR(50),
+    source_endpoint VARCHAR(50),
+    source_url VARCHAR(255),
+    source_element_data JSONB
+);
+
+COMMENT ON TABLE rgts.price_sources IS 'Table to store information about price sources';
+COMMENT ON COLUMN rgts.price_sources.source_id IS 'Unique identifier for each source';
+COMMENT ON COLUMN rgts.price_sources.source_name IS 'Name of the price source';
+COMMENT ON COLUMN rgts.price_sources.source_endpoint IS 'Endpoint of the price source';
+COMMENT ON COLUMN rgts.price_sources.source_url IS 'URL of the price source';
+COMMENT ON COLUMN rgts.price_sources.source_element_data IS 'Data related to the HTML element for extracting prices';
