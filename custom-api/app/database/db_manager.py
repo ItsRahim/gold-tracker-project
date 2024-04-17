af51f6e6cbf8ac40c366d4f1142f59dd784d7096
ItsRahim <rahim1605@gmail.com>
2024-04-17 22:54:13 +0100

    improved exceptio hadling

--- custom-api/app/database/db_manager.py
+++ custom-api/app/database/db_manager.py
@@ -1,7 +1,12 @@
-from sqlalchemy import create_engine, text
-from sqlalchemy.orm import sessionmaker
-from dotenv import load_dotenv
 import os
+from contextlib import contextmanager
+
+from dotenv import load_dotenv
+from sqlalchemy import create_engine, text
+from sqlalchemy.exc import SQLAlchemyError
+from sqlalchemy.orm import sessionmaker
+
+from app.config.logging import log
 
 
 class DatabaseManager:
@@ -16,13 +21,30 @@ class DatabaseManager:
 
         connection_url = f'postgresql://{user}:{password}@{host}:{port}/{dbname}'
 
-        self.engine = create_engine(connection_url)
+        self.engine = create_engine(connection_url, pool_size=10, max_overflow=20)
         self.Session = sessionmaker(bind=self.engine)
 
     def execute_query(self, query, params=None):
-        with self.engine.connect() as conn:
-            result = conn.execute(text(query), params)
-            return result.fetchall()
+        try:
+            with self.engine.connect() as conn:
+                result = conn.execute(text(query), params)
+                return result.fetchall()
+        except SQLAlchemyError as e:
+            log.error(f"Error executing query: {query}. Error: {e}")
+            raise
+
+    @contextmanager
+    def session_scope(self):
+        session = self.Session()
+        try:
+            yield session
+            session.commit()
+        except Exception as e:
+            session.rollback()
+            log.error(f"Error during session operation: {e}")
+            raise
+        finally:
+            session.close()
 
     def close_connection(self):
         self.engine.dispose()
