8a38ce484ee059b1dd25b8f40a1ba60ee86e83fb
ItsRahim <rahim1605@gmail.com>
2024-04-26 18:14:10 +0100

    improved comments

--- gateway-service/src/main/java/com/rahim/gatewayservice/paths/ClientPath.java
+++ gateway-service/src/main/java/com/rahim/gatewayservice/paths/ClientPath.java
@@ -8,25 +8,28 @@ public final class ClientPath {
 
     private ClientPath() {}
 
-    /* ---------------------------------------------
+    /**
+     *  ---------------------------------------------
      *  CONFIG SERVER CLIENT ROUTES
-     * ---------------------------------------------
+     *  ---------------------------------------------
      */
     public static final String JAVA_ENCRYPTION = "/encrypt";
     public static final String PYTHON_ENCRYPTION = JAVA_ENCRYPTION + "/python";
 
-    /* ---------------------------------------------
+    /**
+     *  ---------------------------------------------
      *  ACCOUNT MICROSERVICE CLIENT ROUTES
-     * ---------------------------------------------
+     *  ---------------------------------------------
      */
     public static final String DEFAULT_ACCOUNT_ROUTE = "/account";
     public static final String ACCOUNT_ROUTE = "/account/**";
     public static final String DEFAULT_PROFILE_ROUTE = "/profile";
     public static final String PROFILE_ROUTE = "/profile/**";
 
-    /* ---------------------------------------------
+    /**
+     *  ---------------------------------------------
      *  PRICING MICROSERVICE CLIENT ROUTES
-     * ---------------------------------------------
+     *  ---------------------------------------------
      */
     public static final String DEFAULT_PRICE_ROUTE = "/price";
     public static final String PRICE_ROUTE = "/price/*";
