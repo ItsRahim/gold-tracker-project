af40e88bab3dc473ada58e5e3f9f06b1e98810de
ItsRahim <rahim1605@gmail.com>
2024-05-02 14:37:34 +0100

    fixed bean error and reference method in class instead

--- account-service/src/main/java/com/rahim/accountservice/service/hazelcast/implementation/HazelcastCacheManager.java
+++ account-service/src/main/java/com/rahim/accountservice/service/hazelcast/implementation/HazelcastCacheManager.java
@@ -3,9 +3,10 @@ package com.rahim.accountservice.service.hazelcast.implementation;
 import com.hazelcast.collection.ISet;
 import com.hazelcast.core.HazelcastInstance;
 import com.rahim.accountservice.service.hazelcast.CacheManager;
-import lombok.RequiredArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
 
 /**
@@ -13,27 +14,31 @@ import org.springframework.stereotype.Service;
  * @created 01/05/2024
  */
 @Service
-@RequiredArgsConstructor
 public class HazelcastCacheManager implements CacheManager {
 
     private static final Logger LOG = LoggerFactory.getLogger(HazelcastCacheManager.class);
     private final HazelcastInstance hazelcastInstance;
 
+    @Autowired
+    public HazelcastCacheManager(@Qualifier("hazelcastInstance") HazelcastInstance hazelcastInstance) {
+        this.hazelcastInstance = hazelcastInstance;
+    }
+
     public void addToSet(Object value, String setName) {
         LOG.debug("Adding {} to {} Hazelcast set...", value, setName);
-        ISet<Object> set = hazelcastInstance.getSet(setName);
+        ISet<Object> set = getSet(setName);
         set.add(value);
     }
 
     public void removeFromSet(Object value, String setName) {
         LOG.debug("Removing {} from {} Hazelcast set...", value, setName);
-        ISet<Object> set = hazelcastInstance.getSet(setName);
+        ISet<Object> set = getSet(setName);
         set.remove(value);
     }
 
     public void clearSet(String setName) {
         LOG.debug("Clearing {} Hazelcast set...", setName);
-        ISet<Object> set = hazelcastInstance.getSet(setName);
+        ISet<Object> set = getSet(setName);
         set.clear();
     }
 
