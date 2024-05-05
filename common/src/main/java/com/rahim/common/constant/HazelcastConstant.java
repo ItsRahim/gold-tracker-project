3aa42e6c7adf7b20fa443933ec393fd0cda5aa74
ItsRahim <rahim1605@gmail.com>
2024-05-05 10:30:16 +0100

    removed unused kafka-topic yaml and added comments

--- common/src/main/java/com/rahim/common/constant/HazelcastConstant.java
+++ common/src/main/java/com/rahim/common/constant/HazelcastConstant.java
@@ -13,7 +13,10 @@ public class HazelcastConstant {
      * Sets
      * ----------------------------------------
      */
+    /*Set containing account ids with notifications enabled*/
     public static final String ACCOUNT_ID_SET = "account-id";
+
+    /*Set containing kafka messages which have been processed*/
     public static final String PROCESSED_KAFKA_MESSAGES = "processed-kafka-messages";
 
     /**
@@ -21,5 +24,6 @@ public class HazelcastConstant {
      * Maps
      * ----------------------------------------
      */
+    /*Map containing hazelcast storage initialisation state from various microservices*/
     public static final String HAZELCAST_INITIALISER_MAP = "init-status";
 }
