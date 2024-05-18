bba18270896e27fc7996b1d406cd918e321da9f9
ItsRahim <rahim1605@gmail.com>
2024-05-18 13:00:41 +0100

    added @Tranactional annotation to repsitory class

--- pricing-service/src/main/java/com/rahim/pricingservice/service/repository/implementation/GoldPriceRepositoryHandler.java
+++ pricing-service/src/main/java/com/rahim/pricingservice/service/repository/implementation/GoldPriceRepositoryHandler.java
@@ -14,6 +14,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.dao.DataAccessException;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.NoSuchElementException;
@@ -24,6 +25,7 @@ import java.util.Optional;
  * @created 16/03/2024
  */
 @Service
+@Transactional
 @RequiredArgsConstructor
 public class GoldPriceRepositoryHandler implements IGoldPriceRepositoryHandler {
 
