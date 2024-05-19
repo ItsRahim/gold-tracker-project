cccc2b625de6ad3e092648a3a6a833f518f4e80f
ItsRahim <rahim1605@gmail.com>
2024-05-19 12:01:55 +0100

    added resource files and updated config server

--- investment-service/src/main/java/com/rahim/investmentservice/InvestmentServiceApplication.java
+++ investment-service/src/main/java/com/rahim/investmentservice/InvestmentServiceApplication.java
@@ -2,8 +2,12 @@ package com.rahim.investmentservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.ComponentScan;
 
+@EnableDiscoveryClient
 @SpringBootApplication
+@ComponentScan(basePackages = {"com.rahim.investmentservice", "com.rahim.common"})
 public class InvestmentServiceApplication {
 
     public static void main(String[] args) {
