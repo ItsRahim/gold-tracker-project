a9b73a4ec3ac969bc01baafc2ec61193df133f20
ItsRahim <rahim1605@gmail.com>
2024-05-19 13:56:39 +0100

    added temp get mapping with swagger annotation

--- investment-service/src/main/java/com/rahim/investmentservice/controller/HoldingController.java
+++ investment-service/src/main/java/com/rahim/investmentservice/controller/HoldingController.java
@@ -1,8 +1,17 @@
 package com.rahim.investmentservice.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -15,7 +24,18 @@ import static com.rahim.investmentservice.constants.HoldingControllerEndpoint.BA
 @RestController
 @RequiredArgsConstructor
 @RequestMapping(BASE_URL)
+@Tag(name = "Endpoint to manage user holdings")
 public class HoldingController {
 
     private static final Logger LOG = LoggerFactory.getLogger(HoldingController.class);
+
+    @Operation(summary = "Some summary")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "TODO", content = @Content(mediaType = "text/plain")),
+            @ApiResponse(responseCode = "500", description = "TODO", content = @Content(mediaType = "text/plain"))
+    })
+    @GetMapping(value = "", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<Object> getSomething() {
+        return ResponseEntity.status(HttpStatus.CREATED).body("TODO");
+    }
 }
