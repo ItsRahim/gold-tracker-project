b9cb9e227febafcb535fd3b07dc1662ddbd9ab81
ItsRahim <rahim1605@gmail.com>
2024-06-05 17:45:43 +0100

    moved deploymetns to one compose and fixed acknowldgement issues

--- common/src/main/java/com/rahim/common/config/kafka/KafkaConsumerConfig.java
+++ common/src/main/java/com/rahim/common/config/kafka/KafkaConsumerConfig.java
@@ -8,6 +8,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
 import org.springframework.kafka.core.ConsumerFactory;
 import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
+import org.springframework.kafka.listener.ContainerProperties;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -36,6 +37,7 @@ public class KafkaConsumerConfig {
     public ConcurrentKafkaListenerContainerFactory<String, String> kafkaListenerContainerFactory() {
         ConcurrentKafkaListenerContainerFactory<String, String> factory = new ConcurrentKafkaListenerContainerFactory<>();
         factory.setConsumerFactory(consumerFactory());
+        factory.getContainerProperties().setAckMode(ContainerProperties.AckMode.MANUAL);
 
         return factory;
     }
