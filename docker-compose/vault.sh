92945672b7872cc3d952bbefc4d1d426643ed459
ItsRahim <rahim1605@gmail.com>
2024-06-11 20:44:09 +0100

    added another profile for docker/qa

--- docker-compose/vault.sh
+++ docker-compose/vault.sh
@@ -9,6 +9,6 @@ vault secrets enable -path=kv kv
 
 # Adding secrets
 vault kv put kv/config-server \
-    gold-api-encryption-key=b67a5e0887d90c3b6749b486f5586e54 \
-	encryption.key = \
-	salt.key=da95bc4003b551c6bf0d65a7d2b4bfc543eea89b55c0197bd7c7cb8d4f9912c4
+    gold.api.encryption.key=b67a5e0887d90c3b6749b486f5586e54 \
+    encryption.key=dev_key \
+    salt.key=dev_salt
