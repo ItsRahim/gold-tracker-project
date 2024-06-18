6174f366be9cc155f9df897c01c468d9df3ee3c6
ItsRahim <rahim1605@gmail.com>
2024-06-18 13:19:32 +0100

    added try catch to investment row mapper andchange variable name for price update row mapper

--- investment-service/src/main/java/com/rahim/investmentservice/util/InvestmentRowMapper.java
+++ investment-service/src/main/java/com/rahim/investmentservice/util/InvestmentRowMapper.java
@@ -1,6 +1,8 @@
 package com.rahim.investmentservice.util;
 
 import com.rahim.investmentservice.model.InvestmentResponse;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.jdbc.core.RowMapper;
 
 import java.sql.ResultSet;
@@ -12,12 +14,20 @@ import java.sql.SQLException;
  */
 public class InvestmentRowMapper implements RowMapper<InvestmentResponse> {
 
+    private static final Logger log = LoggerFactory.getLogger(InvestmentRowMapper.class);
+
     @Override
-    public InvestmentResponse mapRow(ResultSet rs, int rowNum) throws SQLException {
+    public InvestmentResponse mapRow(ResultSet rs, int rowNum) {
         InvestmentResponse investmentResponse = new InvestmentResponse();
-        investmentResponse.setCurrentValue(rs.getBigDecimal(1));
-        investmentResponse.setPurchasePrice(rs.getBigDecimal(2));
-        investmentResponse.setPurchaseDate(rs.getDate(3).toLocalDate());
+
+        try {
+            investmentResponse.setCurrentValue(rs.getBigDecimal(1));
+            investmentResponse.setPurchasePrice(rs.getBigDecimal(2));
+            investmentResponse.setPurchaseDate(rs.getDate(3).toLocalDate());
+        } catch (SQLException e) {
+            log.error("Error mapping investment response row", e);
+            throw new RuntimeException("Error mapping investment response row", e);
+        }
 
         return investmentResponse;
     }
