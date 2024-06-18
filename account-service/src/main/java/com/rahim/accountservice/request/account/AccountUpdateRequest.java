4156dcde94c5678cd84d8b129dafb329e89b4099
ItsRahim <rahim1605@gmail.com>
2024-06-18 18:40:16 +0100

    added test for successfull account update

--- account-service/src/main/java/com/rahim/accountservice/request/account/AccountUpdateRequest.java
+++ account-service/src/main/java/com/rahim/accountservice/request/account/AccountUpdateRequest.java
@@ -1,6 +1,8 @@
 package com.rahim.accountservice.request.account;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 /**
@@ -9,6 +11,8 @@ import lombok.Setter;
  */
 @Getter
 @Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class AccountUpdateRequest {
     private String email;
     private String passwordHash;
--- account-service/src/test/java/com/rahim/accountservice/service/account/AccountUpdateSvcTest.java
+++ account-service/src/test/java/com/rahim/accountservice/service/account/AccountUpdateSvcTest.java
@@ -1,7 +1,17 @@
 package com.rahim.accountservice.service.account;
 
+import com.hazelcast.collection.ISet;
 import com.rahim.accountservice.config.AbstractTestConfig;
+import com.rahim.accountservice.config.TestDataGenerator;
+import com.rahim.accountservice.entity.Account;
+import com.rahim.accountservice.model.Address;
+import com.rahim.accountservice.model.UserRequest;
+import com.rahim.accountservice.repository.AccountRepository;
+import com.rahim.accountservice.request.account.AccountCreationRequest;
+import com.rahim.accountservice.request.account.AccountUpdateRequest;
+import com.rahim.accountservice.request.profile.ProfileCreationRequest;
 import com.rahim.accountservice.service.repository.implementation.AccountRepositoryHandlerService;
+import com.rahim.common.constant.HazelcastConstant;
 import com.rahim.common.service.hazelcast.CacheManager;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -11,6 +21,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
+
+import java.util.Optional;
+
+import static org.assertj.core.api.AssertionsForInterfaceTypes.assertThat;
 
 /**
  * Author: Rahim Ahmed
@@ -26,16 +40,83 @@ public class AccountUpdateSvcTest extends AbstractTestConfig {
     private IAccountUpdateService accountUpdateService;
 
     @Autowired
-    private AccountRepositoryHandlerService accountRepositoryHandlerService;
+    private IAccountCreationService accountCreationService;
+
+    @Autowired
+    private AccountRepositoryHandlerService accountRepositoryHandler;
+
+    @Autowired
+    private AccountRepository accountRepository;
 
     @Autowired
     private CacheManager hazelcastCacheManager;
 
+    UserRequest generateData() {
+        Address address = TestDataGenerator.generateAddress();
+        AccountCreationRequest accountCreationRequest = new AccountCreationRequest("metus@icloud.org", "metus123!");
+        ProfileCreationRequest profileCreationRequest = new ProfileCreationRequest("metus", "Stephen", "Drake", "07678584701", address);
+
+        return new UserRequest(accountCreationRequest, profileCreationRequest);
+    }
+
     @Test
     @DisplayName("Successful account update")
     void shouldUpdateAccountSuccessfully() {
-        // Test implementation
+        // Creating initial account
+        UserRequest userRequest = accountCreationService.createAccount(generateData());
+
+        // Verify account id is not present in hazelcast account notification set
+        ISet<Integer> accountIdNotifs = hazelcastCacheManager.getSet(HazelcastConstant.ACCOUNT_ID_NOTIFICATION_SET);
+        assertThat(accountIdNotifs).isNullOrEmpty();
+
+        // Verifying new account has been created
+        assertThat(userRequest).isNotNull();
+
+        // Getting original account
+        Optional<Account> accountOptional = accountRepositoryHandler.getAllAccounts()
+                .stream()
+                .filter(account -> account.getEmail().equals(userRequest.getAccount().getEmail()))
+                .findFirst();
+
+        assertThat(accountOptional).isNotEmpty();
+        Account originalAccount = accountOptional.get();
+        Integer accountId = originalAccount.getId();
+
+        // Creating update account request data
+        AccountUpdateRequest accountUpdateRequest = new AccountUpdateRequest("new.email@gmail.com", "newPassword123!", "true");
+
+        // Performing account update
+        Object updatedObject = accountUpdateService.updateAccount(accountId, accountUpdateRequest);
+
+        // Asserting account id has been added to hazelcast notification set
+        accountIdNotifs = hazelcastCacheManager.getSet(HazelcastConstant.ACCOUNT_ID_NOTIFICATION_SET);
+        assertThat(accountIdNotifs).isNotEmpty();
+
+        // Asserting that the update was successful and returned an Account object
+        assertThat(updatedObject).isInstanceOf(Account.class);
+        Account updatedAccount = (Account) updatedObject;
+
+        String updatedEmail = accountUpdateRequest.getEmail();
+        String updatedPassword = accountUpdateRequest.getPasswordHash();
+        Boolean updatedNotification = Boolean.parseBoolean(accountUpdateRequest.getNotificationSetting());
+
+        // Verifying the updated account details
+        assertThat(updatedAccount.getId()).isEqualTo(accountId);
+        assertThat(updatedAccount.getEmail()).isEqualTo(updatedEmail);
+        assertThat(updatedAccount.getPasswordHash()).isEqualTo(updatedPassword);
+        assertThat(updatedAccount.getNotificationSetting()).isEqualTo(updatedNotification);
+
+        // Reload the account from the repository to verify persistence
+        Optional<Account> reloadedAccountOptional = accountRepository.findById(accountId);
+        assertThat(reloadedAccountOptional).isPresent();
+        Account reloadedAccount = reloadedAccountOptional.get();
+
+        // Asserting that reloaded account matches the updated account
+        assertThat(reloadedAccount.getEmail()).isEqualTo(updatedEmail);
+        assertThat(reloadedAccount.getPasswordHash()).isEqualTo(updatedPassword);
+        assertThat(reloadedAccount.getNotificationSetting()).isEqualTo(updatedNotification);
     }
+
 
     @Test
     @DisplayName("No Updates Applied")
